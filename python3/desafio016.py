df3ddfba65bba334c6c611a3324704cf1d96577d
RafaelCardozo <rafaelcardosodoamaral.adv@gmail.com>
2018-06-04 12:28:07 -0300

    Resolução do desafio 016

---
+++ python3/desafio016.py
@@ -0,0 +1,6 @@
+# _*_ coding:utf-8 _*_
+
+dias = int(input('Quantos dias alugados? '))
+km = float(input('Quantos Km rodados? '))
+pago = (dias * 60) + (km * 0.15)
+print('O total a pagar é de R${:.2f}'.format(pago))
