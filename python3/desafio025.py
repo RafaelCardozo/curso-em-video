1007466427307ae84af0e88d3dc420b91d54b842
RafaelCardozo <rafaelcardosodoamaral.adv@gmail.com>
2018-06-12 14:44:15 -0300

    Resolução dos Desafios.

---
+++ python3/desafio025.py
@@ -0,0 +1,3 @@
+"""Vigésimo Quinto Desafio."""
+cid = str(input('Em que cidade você nasceu? ')).strip()
+print(cid[:5].upper == 'SANTO')
---
+++ python3/desafio026.py
@@ -0,0 +1,3 @@
+"""Vigésimo Sexto Desafio."""
+nome = str(input('Qual é seu nome completo? ')).strip()
+print('Seu nome tem Silva? {}'.format('SILVA' in nome.upper()))
