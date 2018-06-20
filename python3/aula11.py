0bbe19b673b3844e5a90b39e416d102876e87fff
RafaelCardozo <rafaelcardosodoamaral.adv@gmail.com>
2018-06-20 18:27:05 -0300

    Adicionado Aula11 - Corese no Terminal Codigo ANSI

---
+++ python3/aula11.py
@@ -0,0 +1,5 @@
+"""Aula 11 - fase prática."""
+print('\033[1;31;43m Olá Mundo!\033[m')
+a = int(input('\033[7;37;40mDigite o primeiro número:\033[m '))
+b = int(input('Digite o segundo valor: '))
+print('Os valores são \033[32m{}\033[m e \033[31m{}\033[m'.format(a, b))
