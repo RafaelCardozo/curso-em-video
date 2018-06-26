fd51eb173157459b76daaf0116006fd6ed6fe91a
RafaelCardozo <rafaelcardosodoamaral.adv@gmail.com>
2018-06-26 19:02:04 -0300

    Adicionado a aula #13.

---
+++ python3/mundo2/aula13.py
@@ -0,0 +1,10 @@
+"""Aula #13 - Parte prática"""
+for c in range(0, 6):
+    print('Oi!')
+print('FIM')
+i = int(input('Inicio'))
+f = int(input('Final'))
+p = int(input('Passo'))
+for c in range(i, f + 1, p):
+    print(c)
+print('FIM')
