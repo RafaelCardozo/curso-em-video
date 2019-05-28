e8ce442cc9479895d24e0f19283ce3778282db7f
Rafael Cardoso <rafaelcardosodoamaral.adv@gmail.com>
2019-05-28 09:09:02 -0300

    Adicionado aula 17

---
+++ python3/mundo3/aula17.py
@@ -0,0 +1,17 @@
+u"""AULA PRÁTICA017.
+
+Curso em Vídeo - Professor Gustavo Guanabara.
+"""
+num = [2, 5, 9, 1]
+print(num)
+num[2] = 3
+print(num)
+num.append(7)
+print(num)
+num.sort(reverse=True)
+print(num)
+num.insert(2, 0)
+print(num)
+num.pop(2)
+print(num)
+print(f'Essa lista tem {len(num)} elementos.')
