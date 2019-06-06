568a813b58e5dff5acde5d116fb0f4c00c340e59
Rafael Cardoso <rafaelcardosodoamaral.adv@gmail.com>
2019-06-06 07:52:12 -0300

    Adicionado mais conteúdo da aula.

--- python3/mundo3/aula17.py
+++ python3/mundo3/aula17.py
@@ -8,10 +8,28 @@ num[2] = 3
 print(num)
 num.append(7)
 print(num)
+num.sort()
+print(num)
 num.sort(reverse=True)
 print(num)
 num.insert(2, 0)
 print(num)
 num.pop(2)
 print(num)
+if 4 in num:
+    num.remove(4)
+else:
+    print('Não achei o número 4')
+print(num)
 print(f'Essa lista tem {len(num)} elementos.')
+
+valores = list()
+valores.append(5)
+valores.append(9)
+valores.append(4)
+
+for v in valores:
+    print(f'{v}...')
+for c, v in enumerate(valores):
+    print(f'Na posição {c} encomtrei o valor {v}!')
+print('Cheguei ao final da lista.')
