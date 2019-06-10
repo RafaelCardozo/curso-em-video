efc8f10641b5e43aeb8b1239c1103b5778b4ae21
Rafael Cardoso <rafaelcardosodoamaral.adv@gmail.com>
2019-06-10 08:10:26 -0300

    melhoramento da estética.

--- python3/mundo3/aula17.py
+++ python3/mundo3/aula17.py
@@ -29,7 +29,7 @@ valores.append(9)
 valores.append(4)
 
 for v in valores:
-    print(f'{v}...')
+    print(f'{v}...', end='')
 for c, v in enumerate(valores):
     print(f'Na posição {c} encomtrei o valor {v}!')
 print('Cheguei ao final da lista.')
