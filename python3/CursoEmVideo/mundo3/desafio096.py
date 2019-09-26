d49b60ae95af8682616619e2825d44cc6cb68a93
Rafael Cardoso <rafaelcardosodoamaral.adv@gmail.com>
2019-09-26 08:39:04 -0300

    resolução pessoal

---
+++ python3/CursoEmVideo/mundo3/desafio096.py
@@ -0,0 +1,19 @@
+u"""DESAFIO096.
+
+Curso em Vídeo - Professor Gustavo Guanabara
+Módulo 3 - Funções(Parte1)
+
+Faça um programa que tenha uma função chamada área(), que receba as dimensões
+de um terreno retangular (largura e comprimento) e mostre a área do terreno.
+"""
+
+
+def área(l, c):
+    area = larg * comp
+    print(area)
+
+
+a = área
+larg = float(input('Largura do terreno: '))
+comp = float(input('Comprimento do terreno: '))
+print(f'O terreno tem as medidas {larg}x{comp} e área de {a}m²')
