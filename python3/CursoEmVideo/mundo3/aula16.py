c830cad2c96cedfc53eedba82ce3a806267766ad
Rafael Cardoso <rafaelcardosodoamaral.adv@gmail.com>
2019-10-17 09:33:35 -0300

    Adicionado mais exemplos práticos.

--- python3/CursoEmVideo/mundo3/aula16.py
+++ python3/CursoEmVideo/mundo3/aula16.py
@@ -14,4 +14,19 @@ for cont in range(0, len(lanche)):
 for pos, comida in enumerate(lanche):
     print(f'Eu vou comer {comida} na posição {pos}.')
 
+print(sorted(lanche))  # coloca em ordem
+print(lanche)
+
 print('Comi pra caramba!')
+
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = b + a
+print(c)
+print(c.count(5))
+print(c.index(2))
+
+pessoa = ('Gustavo', 39, 'M', 99.88)
+print(pessoa)
+# del(pessoa)
+print(pessoa)
