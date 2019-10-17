c6032f8f4a0e48ece9b26d65faf228251847ffa1
Rafael Cardoso <rafaelcardosodoamaral.adv@gmail.com>
2019-10-17 12:01:22 -0300

    Adicionado o enunciado do desafio.

--- python3/CursoEmVideo/mundo1/desafio003.py
+++ python3/CursoEmVideo/mundo1/desafio003.py
@@ -1,4 +1,7 @@
-"""Terceiro Desafio."""
+u"""Desafio 003.
+
+Crie um script Python que lei dois números e tente mostrar a soma entre eles.
+"""
 dia = input('Qual dia você nasceu? ')
 mes = input('Qual o mês que você nasceu? ')
 ano = input('Qual o ano que você nasceu? ')
