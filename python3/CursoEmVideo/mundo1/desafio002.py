b3575e6610781d4716ed96bb96de09a02f032a9a
Rafael Cardoso <rafaelcardosodoamaral.adv@gmail.com>
2019-10-17 12:00:02 -0300

    Adicionado o enunciado do desafio.

--- python3/CursoEmVideo/mundo1/desafio002.py
+++ python3/CursoEmVideo/mundo1/desafio002.py
@@ -1,3 +1,7 @@
-"""Segundo Desafio."""
+u"""Desafio 002.
+
+Crie um script Pyhton que leia o dia, o mês e o ano de nascimento de uma
+ pessoa e  mostre uma mensagem com a data formatada.
+"""
 nome = input('Qual é o seu nome? ')
 print('Olá {}! É um prazer te conhecer.'.format(nome))
