8c6c2edcb1d7f7d856de7ac8ce0b6d79c11a54c9
Rafael Cardoso <rafaelcardosodoamaral.adv@gmail.com>
2019-10-17 09:23:36 -0300

    Adicionado pontuação no final das frases.

--- python3/CursoEmVideo/mundo3/aula16.py
+++ python3/CursoEmVideo/mundo3/aula16.py
@@ -6,12 +6,12 @@ lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita'
 print(lanche)
 
 for comida in lanche:
-    print(f'Eu vou comer {comida}')
+    print(f'Eu vou comer {comida}.')
 
 for cont in range(0, len(lanche)):
-    print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+    print(f'Eu vou comer {lanche[cont]} na posição {cont}.')
 
 for pos, comida in enumerate(lanche):
-    print(f'Eu vou comer {comida} na posição {pos}')
+    print(f'Eu vou comer {comida} na posição {pos}.')
 
 print('Comi pra caramba!')
