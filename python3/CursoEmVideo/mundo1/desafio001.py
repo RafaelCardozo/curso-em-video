5c0afe70b4679f95efd2aeaf04989273867795c6
Rafael Cardoso <rafaelcardosodoamaral.adv@gmail.com>
2019-10-17 11:57:41 -0300

    Adiciona o enunciado do desafio.

--- python3/CursoEmVideo/mundo1/desafio001.py
+++ python3/CursoEmVideo/mundo1/desafio001.py
@@ -1,3 +1,7 @@
-"""Primeiro Desafio."""
+u"""Desafio 001.
+
+Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de
+ boas-vindas de acordo com o valor digitado.
+"""
 msg = 'Olá, Mundo!'
 print(msg)
