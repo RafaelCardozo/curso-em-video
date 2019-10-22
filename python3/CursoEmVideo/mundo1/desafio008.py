5bd33208703cf8fcc350596909f3e6182c1514ee
Rafael Cardoso <rafaelcardosodoamaral.adv@gmail.com>
2019-10-22 10:10:06 -0300

    Formatado a resolução do desafio.

--- python3/CursoEmVideo/mundo1/desafio008.py
+++ python3/CursoEmVideo/mundo1/desafio008.py
@@ -4,4 +4,4 @@ Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua 
 """
 n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digite a segunda nota: '))
-print('A média do aluno foi {:.1f}'.format((n1 + n2)/2))
+print('A média do aluno foi {:.1f}'.format((n1+n2)/2))
