a40b70c615d73ee230930835e05a18f129a6c1cf
Rafael Cardoso <rafaelcardosodoamaral.adv@gmail.com>
2019-10-22 10:49:58 -0300

    Acrescentado o TODO.

--- python3/CursoEmVideo/mundo1/desafio011.py
+++ python3/CursoEmVideo/mundo1/desafio011.py
@@ -6,3 +6,5 @@ Considere - US$1,00 = R$3,27
 c = float(input('Quanto dinheiro você tem na carteira? R$'))
 d = 3.27
 print('Com R${:.2f}, podes comprar US${:.2f}'.format(c, c/d))
+
+# TODO: Acrescentar a conversão para Yene, Euro, Libra Isterlina, Peso Colombiano, e todos os paises que já fui.
