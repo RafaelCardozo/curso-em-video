3b3d14f17675d7121a6fd945d27df730d3dd634d
Rafael Cardoso <rafaelcardosodoamaral.adv@gmail.com>
2019-10-22 09:58:02 -0300

    Formatado o enunciado.

--- python3/CursoEmVideo/mundo1/desafio006.py
+++ python3/CursoEmVideo/mundo1/desafio006.py
@@ -1,7 +1,6 @@
 u"""Desafio006.
 
-Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e
- seu antecessor.
+Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
 """
 n = int(input('Digite um número inteiro: '))
 print('O número digitado foi {}, o número antecessor é {}, o número sucessor é {}'.format(n, (n-1), (n+1)))
