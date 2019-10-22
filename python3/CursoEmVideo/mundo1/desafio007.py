3047fa78cd212624aee65b904250a810ce06717c
Rafael Cardoso <rafaelcardosodoamaral.adv@gmail.com>
2019-10-22 10:09:17 -0300

    Formatado a resulução do desafio.

--- python3/CursoEmVideo/mundo1/desafio007.py
+++ python3/CursoEmVideo/mundo1/desafio007.py
@@ -3,4 +3,4 @@ u"""Desafio007.
 Crie um algoritmo que leia um número e mostre seu dobro, seu triplo e a raiz quadrada.
 """
 n = int(input('Digite um número: '))
-print('O número digitado foi {}.\n Seu dobro é {}.\n Seu triplo é {}.\n Sua raiz quadrada é {:.2f}'.format(n, n * 2, n * 3, n**(1 / 2)))
+print('O número digitado foi {}.\n Seu dobro é {}.\n Seu triplo é {}.\n Sua raiz quadrada é {:.2f}'.format(n, n*2, n*3, n**(1/2)))
